bed1ce8a0308287b16d2472f039e6f0aabbc012c
Jose Szychowski <joseszychowski@gmail.com>
2024-02-12 13:20:24 -0300

    feat: create a test model email

---
+++ src/services/email/models/testModel.ts
@@ -0,0 +1,6 @@
+import { IEmailModel } from '../interfaces/model.interface';
+
+export const testModel: IEmailModel = {
+    subject: 'This a email test subject',
+    htmlBody: '<strong>This is a email test html body</strong>',
+};
