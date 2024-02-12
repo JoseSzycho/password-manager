8f51aab78ae938f4df2e520b0c92a4e7d0b190bc
Jose Szychowski <joseszychowski@gmail.com>
2024-02-12 13:17:50 -0300

    chore: rename file

---
+++ src/services/email/providers/gmailProvider.ts
@@ -0,0 +1,11 @@
+import { SMTPProvider } from './SMTPProvider';
+import 'dotenv/config';
+
+const gmailProvider = new SMTPProvider(
+    'smtp.gmail.com',
+    465,
+    process.env.GMAIL_PROVIDER_USER || '',
+    process.env.GMAIL_PROVIDER_PASSWORD || ''
+);
+
+export { gmailProvider        };
