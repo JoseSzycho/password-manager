bfbda4776860977ac24ecfb559d6ca9da6f07b25
Jose Szychowski <joseszychowski@gmail.com>
2024-02-13 19:19:23 -0300

    feat: add use of db and add registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,8 +12,19 @@ class AuthController {
     signUp = async (req: Request, res: Response, next: NextFunction) => {
         const user: IUser = req.body;
         try {
-            const data = await this.authService.signUp(user);
-            res.status(201).json(data);
+            await this.authService.signUp(user);
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    register = async (req: Request, res: Response, next: NextFunction) => {
+        const jwt = req.query.jwt as string;
+
+        try {
+            const user = await this.authService.register(jwt);
+            res.status(201).json(user);
         } catch (error) {
             next(error);
         }
