8572500009d0797822b31df25048eb7bc9cedc63
Jose Szychowski <joseszychowski@gmail.com>
2024-02-13 14:50:46 -0300

    feat: add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { authRouter } from './auth/auth.route';
+import { errorHandler } from './middleware';
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRouter);
+app.use(errorHandler);
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
