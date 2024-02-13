9cf14587fb32aba38ae2bba6a115b8f6173c0645
Jose Szychowski <joseszychowski@gmail.com>
2024-02-13 14:57:58 -0300

    change: remove verbose configuration in ajv

--- src/middleware/validateReqBody.middleware.ts
+++ src/middleware/validateReqBody.middleware.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { JSONSchemaType } from 'ajv';
 import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
+import { BadRequestException } from '../errors';
 
 export const validateReqBody = (schema: JSONSchemaType<unknown>) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        const ajv = new Ajv({ allErrors: true, verbose: true });
+        const ajv = new Ajv({ allErrors: true });
         addFormats(ajv, ['email']);
         const validate = ajv.compile(schema);
 
