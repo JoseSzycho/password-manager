839f69f28bdc7b1bf5190988645a99c1d8d8dbc0
Jose Szychowski <joseszychowski@gmail.com>
2024-02-14 20:01:41 -0300

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { authRouter } from './auth/auth.route';
 import { errorHandler } from './middleware';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/auth', authRouter);
+
 app.use(errorHandler);
 
 const PORT = 3000;
