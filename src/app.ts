3fb82f52b10129929c167b451e816a1890edd187
Jose Szychowski <joseszychowski@gmail.com>
2024-02-15 11:16:40 -0300

    feat: add body parser to handle url-encoded

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { authRouter } from './auth/auth.route';
 import { errorHandler } from './middleware';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/auth', authRouter);
 
