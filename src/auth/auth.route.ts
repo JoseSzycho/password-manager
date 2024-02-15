0e6daa81f0735c173bd5a8faeb3f16b1b703496d
Jose Szychowski <joseszychowski@gmail.com>
2024-02-15 13:20:16 -0300

    feat: add login-request route

--- src/auth/auth.route.ts
+++ src/auth/auth.route.ts
@@ -1,11 +1,16 @@
 import express, { Router } from 'express';
 import { authController } from './auth.controller';
 import { validateReqBody } from '../middleware';
-import { userSchema } from './schema';
+import { emailSchema, userSchema } from './schema';
 
 const router: Router = express.Router();
 
 router.post('/signup', validateReqBody(userSchema), authController.signUp);
 router.post('/register', authController.register);
+router.post(
+    '/login-request',
+    validateReqBody(emailSchema),
+    authController.loginRequest
+);
 
 export { router as authRouter };
