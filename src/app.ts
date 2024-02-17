ff8c418edf61efc1a51556082960d98255aa6ba1
Jose Szychowski <joseszychowski@gmail.com>
2024-02-17 08:35:02 -0300

    feat: implemenet cookieParser middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { authRouter } from './auth/auth.route';
 import { errorHandler } from './middleware';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_PRIVATE_KEY));
 
 app.use('/auth', authRouter);
 
